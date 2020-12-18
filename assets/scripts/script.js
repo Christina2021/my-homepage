0e71bee173cb6cddb95e08026e69249e93fe3b29
Christina2021 <65867599+Christina2021@users.noreply.github.com>
2020-12-17 22:51:58 -0800

    Updated weekly events to textareas.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,6 +2,7 @@
 
 
 //Edit button to update prompts
+let currentHour = moment().startOf('hour');
 
 //Moment.js for time (setInterval used to be in real-time)
 function currentTime() {
@@ -11,15 +12,34 @@ function currentTime() {
     if(moment().startOf('second').isSame(moment().startOf('hour'))){
         //Update currentHour if changed;
         currentHour = moment().startOf('hour');
-        updatePastPresentFuture(currentHour);
+        updateGreeting(currentHour);
     };
 };
 setInterval(currentTime, 1000);
 //Moment.js for greeting to change based on the time
-
+function updatePastPresentFuture(hour) {
+    let lateEnd = moment().set('hour', 3).startOf('hour');
+    let earlyEnd = moment().set('hour', 6).startOf('hour');
+    let morningEnd = moment().set('hour', 12).startOf('hour');
+    let afternoonEnd = moment().set('hour', 17).startOf('hour');
+    let eveningEnd = moment().set('hour', 24).startOf('hour');
+    
+    if (hour.isBefore(lateEnd)){
+        $('#specific-greeting').html(`It's late, `)
+    } else if (hour.isBefore(earlyEnd)){
+        $('#specific-greeting').html(`It's early, `)
+    } else if (hour.isBefore(morningEnd)){
+        $('#specific-greeting').html(`Good Morning, `)
+    } else if (hour.isBefore(afternoonEnd)){
+        $('#specific-greeting').html(`Good Afternoon, `)
+    } else if (hour.isBefore(eveningEnd)){
+        $('#specific-greeting').html(`Good Evening, `)
+    } ;
+};
+updatePastPresentFuture(currentHour);
 
 //Weather - api stuff
-
+//<p id="weather-today">The weather today will be</p>
 
 //To-do list
 //add to-do
