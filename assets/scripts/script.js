8d456d220dc9f8f6e84a4e4e5a23d7260d6bd3d9
Christina2021 <65867599+Christina2021@users.noreply.github.com>
2020-12-18 12:14:50 -0800

    Updated with weather data.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -6,7 +6,7 @@ let currentHour = moment().startOf('hour');
 
 //Moment.js for time (setInterval used to be in real-time)
 function currentTime() {
-    $('#current-time').html(moment().format("dddd LL h[:]mm[:]ss A"));
+    $('#current-time').html(moment().format("[~]dddd LL h[:]mm[:]ss A[~]"));
 
     //Update past/present/future colors if start of new hour
     if(moment().startOf('second').isSame(moment().startOf('hour'))){
@@ -40,6 +40,42 @@ updatePastPresentFuture(currentHour);
 
 //Weather - api stuff
 //<p id="weather-today">The weather today will be</p>
+
+/*
+let myKey = prompt("Please enter in your API key for O");
+let cityAPIName = prompt("What city do you live in?");
+
+let currentURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityAPIName + "&units=imperial&appid=" + myKey;
+*/
+
+function getWeather() {
+    let currentURL = "https://api.openweathermap.org/data/2.5/weather?q=san+diego&units=imperial&appid=";
+
+    //First API call for lat/lon of city to use for second API call
+    $.ajax({
+        url: currentURL,
+        method: "GET",
+        //If city is not valid, will give an error message
+        error: function() {
+            alert("City name entered is not valid.  Please enter in a valid city name.");
+            return;
+        }
+    })
+    .then(function(response){
+        let city = response.name;
+        let currentTemp = response.main.temp;
+        let currentHigh = response.main.temp_max;
+        let currentLow = response.main.temp_min;        
+        let currentWeatherCondition = response.weather[0].description;
+        
+        $('#weather-today').html(`The current weather in ${city} is ${currentTemp}&#8457; (${currentWeatherCondition}), with a high of ${currentHigh}&#8457; and low of ${currentLow}&#8457;!`);
+    });
+}
+getWeather();
+
+
+
+
 
 //To-do list
 //add to-do
