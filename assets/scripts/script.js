4244a979d83d0b765cca43d7f8d4c90151d9976f
Christina2021 <65867599+Christina2021@users.noreply.github.com>
2020-12-18 20:34:13 -0800

    Updated with local storage for name and weather.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,12 +1,20 @@
 //Any variables
-
+let myName = "";
+let myWeather;
+let myKey;
+let cityName;
 
 //Edit button to update prompts
+//modal?
+
+
+
+
 let currentHour = moment().startOf('hour');
 
 //Moment.js for time (setInterval used to be in real-time)
 function currentTime() {
-    $('#current-time').html(moment().format("[~]dddd LL h[:]mm[:]ss A[~]"));
+    $('#current-time').html(moment().format("[~ ]dddd LL h[:]mm[:]ss A[ ~]"));
 
     //Update past/present/future colors if start of new hour
     if(moment().startOf('second').isSame(moment().startOf('hour'))){
@@ -35,6 +43,8 @@ function updatePastPresentFuture(hour) {
     } else if (hour.isBefore(eveningEnd)){
         $('#specific-greeting').html(`Good Evening, `)
     } ;
+
+    //Add for weekly dates
 };
 updatePastPresentFuture(currentHour);
 
@@ -44,12 +54,10 @@ updatePastPresentFuture(currentHour);
 /*
 let myKey = prompt("Please enter in your API key for O");
 let cityAPIName = prompt("What city do you live in?");
-
-let currentURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityAPIName + "&units=imperial&appid=" + myKey;
 */
 
-function getWeather() {
-    let currentURL = "https://api.openweathermap.org/data/2.5/weather?q=san+diego&units=imperial&appid=";
+function getWeather(cityAPIName, myKey) {
+    let currentURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityAPIName + "&units=imperial&appid=" + myKey;
 
     //First API call for lat/lon of city to use for second API call
     $.ajax({
@@ -57,7 +65,7 @@ function getWeather() {
         method: "GET",
         //If city is not valid, will give an error message
         error: function() {
-            alert("City name entered is not valid.  Please enter in a valid city name.");
+            alert("There was an issue with either your OpenWeather API key or City Name.  Please use the edit button to update the information.");
             return;
         }
     })
@@ -71,7 +79,8 @@ function getWeather() {
         $('#weather-today').html(`The current weather in ${city} is ${currentTemp}&#8457; (${currentWeatherCondition}), with a high of ${currentHigh}&#8457; and low of ${currentLow}&#8457;!`);
     });
 }
-getWeather();
+
+
 
 
 
@@ -91,7 +100,72 @@ getWeather();
 //if event passed, different color
 
 
-//Prompts for name, API key for openweathermap
-//Local storage for storing name, api key
+//Prompts for name, API key for openweathermap (change to modal later possibly)
+function FirstTimeUse() {
+    let storeSavedInformation = [];
+    let savedInformation;
 
-//Local storage pulls name, api key, to-do's, and weekly
+    alert("Welcome! Please enter in some information so that your homepage may display the necessary information:");
+    myName = prompt("Please enter in your name:");
+    myWeather = confirm("Would you like to add weather data to your homepage (please note that you will need an API key for OpenWeather to utilize this feature - you may sign up for a key at: https://openweathermap.org/)");
+    if (myWeather) {
+        myKey = prompt("Please enter in your OpenWeather API key (this will be saved to your local storage):");
+        cityName = prompt("Please enter in the name of the city that you would like the weather data for:");
+
+        let cityNameRevised = cityName.trim().toLowerCase().split(' ').join('+');
+        getWeather(cityNameRevised, myKey);
+    } else {
+        myKey = false;
+        cityName = false;
+    }
+
+    //Local Storage
+    savedInformation = {name: myName, weather: myWeather, apiKey: myKey, city: cityName};
+    storeSavedInformation = JSON.parse(localStorage.getItem("homepageSavedInformation"));
+
+    //Saves object
+    storeSavedInformation = [];
+    storeSavedInformation[0] = savedInformation;
+
+    //Saves to Local Storage
+    localStorage.setItem("homepageSavedInformation",JSON.stringify(storeSavedInformation));
+    
+
+    //Set name field in html
+    $('#name').html(myName);
+
+};
+
+function displaySavedInformation() {
+    let storeSavedInformation = JSON.parse(localStorage.getItem("homepageSavedInformation"));
+
+    if(!storeSavedInformation || !storeSavedInformation[0].name) {
+        FirstTimeUse();
+        $("#container").removeClass("hidden");
+
+    } else {
+
+        myName = storeSavedInformation[0].name;
+        myWeather = storeSavedInformation[0].weather;
+        myKey = storeSavedInformation[0].apiKey;
+        cityName = storeSavedInformation[0].city;
+    
+        if (myWeather) {
+            let cityNameRevised = cityName.trim().toLowerCase().split(' ').join('+');
+            getWeather(cityNameRevised, myKey);
+        }
+    
+        //Set name field in html
+        $('#name').html(myName);
+
+
+        $("#container").removeClass("hidden");
+    }
+};
+
+displaySavedInformation();
+
+
+
+
+//Local storage pulls to-do's and weekly
